4b7f9421a3300e01549c2894395a7c2ee211b78d
Irina Herzberg <rirarum@yandex.com>
2021-07-01 22:03:52 +0200

    Layout of the Main except for footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css';
+import headerLogoPath from '../../images/header-logo.svg';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={headerLogoPath} alt="Логотип учетной записи"/>
+      <div className="header__links">
+        <Link to="/signup" className="header__link">Регистрация</Link>
+        <Link to="/signin" className="header__link header__link_calltoaction">Войти</Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
